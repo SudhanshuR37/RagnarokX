80ed466402c00c4060993c5c2d42c119505099fd
Sudhanshu Randive <s37randive@gmail.com>
2022-02-09 12:29:14 +0530

    Default Structure of the App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Loader from "./components/Loader";
+import Services from "./components/Services";
+import Transactions from "./components/Transactions";
+import Welcome from "./components/Welcome";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div >
-  )
-}
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar></Navbar>
+        <Welcome></Welcome>
+      </div>
+      <Services></Services>
+      <Transactions></Transactions>
+      <Footer></Footer>
+    </div>
+  );
+};
 
-export default App
+export default App;
